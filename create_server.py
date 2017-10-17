dc127ee6598236265664c344566f93f2667a4158
Anderson <a141890@gmail.com>
2017-10-17 13:58:51 +0900

    Add GPU Memory limitation

--- create_server.py
+++ create_server.py
@@ -9,6 +9,9 @@ args = parser.parse_args()
 
 cluster_spec = json.load(open('config.json', 'rt'))
 cluster = tf.train.ClusterSpec(cluster_spec)
-server = tf.train.Server(cluster, job_name=args.job, task_index=args.task)
+
+gpu_options = tf.GPUOptions(per_process_gpu_memory_fraction=0.2, allow_growth=True)
+server = tf.train.Server(cluster, job_name=args.job, task_index=args.task,
+                         config=tf.ConfigProto(gpu_options=gpu_options))
 server.start()
 server.join()
